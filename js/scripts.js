f743566a53abea5c3e82c5f887d720f6dcbdff68
Student <example@example.com>
2018-10-12 15:22:53 -0700

    add basic image rotation code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+var backgroundRotation = new Array();
+
+backgroundRotation[0] = new Image();
+backgroundRotation[0].src = img/snowylandscape.jpg;
+
+function nextImage(element) {
+  var img = document.getElementById(element);
+  for(var i = 0;i < backgroundRotation.length;i++) {
+    if(imgArray[i].src == img.src) {
+      if(i === imgArray.length) {
+        document.getElementById(element).src = imgArray[0].src;
+        break;
+      }
+      document.getElementById(element).src = imgArray[i+1].src;
+    }
+  }
+}
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var input = $("input#inputText").val();
