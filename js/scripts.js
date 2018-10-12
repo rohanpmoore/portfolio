56da5da15e2b303dbeeec1898c15e7743cd5e877
Student <example@example.com>
2018-10-12 13:26:37 -0700

    remove calculator functionality, add text input box

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-}
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-}
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-}
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-}
+$(document).ready(function() {
+  $("formOne").submit(function(event) {
+    var input = $("input#input-text").val();
+    $(".output-text").text(input);
+    event.preventDefault();
+  })
+})
