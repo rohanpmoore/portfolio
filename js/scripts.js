f986b37d69205030a0cfd87cdd4dfd1e3f657cb5
Student <example@example.com>
2018-10-12 15:43:06 -0700

    continue work on rotating images

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 var backgroundRotation = new Array();
 
 backgroundRotation[0] = new Image();
-backgroundRotation[0].src = img/snowylandscape.jpg;
+backgroundRotation[0].src = img/snowy-landscape.jpg;
+
+backgroundRotation[1] = new Image();
+backgroundRotation[1] = img/snowy-mountain.jpg
 
 function nextImage(element) {
   var img = document.getElementById(element);
+  var newPic = 0;
   for(var i = 0;i < backgroundRotation.length;i++) {
     if(imgArray[i].src == img.src) {
-      if(i === imgArray.length) {
-        document.getElementById(element).src = imgArray[0].src;
-        break;
+      if(i == backgroundRotation.length) {
+        newPic = backgroundRotation[0].src;
+        document.getElementById(element).src = newPic;
       }
-      document.getElementById(element).src = imgArray[i+1].src;
+      if(i != backgroundRotation.length) {
+        newPic = backgroundRotation[i+1].src;
+        document.getElementById(element).src = newPic;
+      }
     }
   }
+  setTimeout(nextImage(document.getElementById(newPic)), 10000);
 }
+
+window.onload = nextImage(snowy-mountain.jpg);
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
